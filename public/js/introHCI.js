d6018b251fbe0002dd2e5c8fec83e08ccbce189d
Mark H <mmhwang@ucsd.edu>
2016-02-11 21:43:02 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+	$('.project a').click(addProjectDetails );
 
 	$('#colorBtn').click(randomizeColors);
 }
@@ -18,6 +18,8 @@ function initializePage() {
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+	
+
 	// Prevent following the link
 	e.preventDefault();
 
@@ -27,7 +29,32 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var urlString = "/project/" + idNumber;
+	console.log(urlString);
+	$.get(urlString, callbackEmpty); // results of the GET get passed into callbackEmpty
 }
+
+/*
+ * Called from addProjectDetails
+ */
+function callbackEmpty(result) {
+	console.log(result);
+	var projectHTML = '<a href-"#" class="thumbnail">' + 
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' + 
+		'<p><small>' + result['date'] + '</small></p></a>' + 
+		result['summary'];
+	console.log(projectHTML);
+
+	var selectorString = "#project" + result['id'] + " .details";
+	$(selectorString).html(projectHTML);
+	console.log(selectorString);
+
+}
+
+/* {id: 6, title: "Meat on the Bones", date: "February 13", summary: "<p>Functionality functionality functionality! Cont…u to complete the interaction flow this week.</p>", image: "http://regmedia.co.uk/2011/03/01/node_code.png"} */
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
